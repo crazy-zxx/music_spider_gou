2044f26fe35b3b8e833e4fa7c6a3b9d1afe01d71
xylx <1934109821@qq.com>
2021-04-08 11:53:41 +0800

    update

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import time
 import re
 import execjs
 
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
 }
